c994faaee4414c8e58f4d0ed73f8c8e0add8647b
Sanatramesh <sanat.r@iiitb.org>
2019-03-16 18:30:25 +0530

    Updated model architecture for training cifar-10 classifier

--- model.py
+++ model.py
@@ -1,43 +1,32 @@
-import os
-import sys
-import time
-import datetime
-import argparse
 import numpy as np
-from random import shuffle
 import matplotlib.pyplot as plt
-from scipy.misc import imread, imsave
-from collections import OrderedDict
 
 import torch as th
-from torch.autograd import Variable
-from torchvision import transforms, utils
 import torch.utils.model_zoo as model_zoo
-from torch.utils.data import Dataset, DataLoader
 
 
-class TDNet_VGG11_Model(th.nn.Module):
+class PCamNetVGGModel(th.nn.Module):
 
-    def __init__(self, freeze_encoder = False):
-        super(TDNet_VGG11_Model, self).__init__()
+    def __init__(self, num_classes=10, freeze_encoder = False):
+        super(PCamNetVGGModel, self).__init__()
         self.vgg_encoder = th.nn.Sequential(
-                            th.nn.Conv2d(3, 64, 3, padding = 1),
-                            th.nn.ReLU(),
-                            th.nn.Conv2d(64, 64, 3, padding = 1),
-                            th.nn.ReLU(),
-                            th.nn.MaxPool2d(2, stride = 2),
+                            th.nn.Conv2d(3, 64, kernel_size=3, padding=1),
+                            th.nn.ReLU(inplace=True),
+                            th.nn.Conv2d(64, 64, kernel_size=3, padding=1),
+                            th.nn.ReLU(inplace=True),
+                            th.nn.MaxPool2d(kernel_size=2, stride=2),
 
-                            th.nn.Conv2d(64, 128, 3, padding = 1),
-                            th.nn.ReLU(),
-                            th.nn.Conv2d(128, 128, 3, padding = 1),
-                            th.nn.ReLU(),
-                            th.nn.MaxPool2d(2, stride = 2),
+                            th.nn.Conv2d(64, 128, kernel_size=3, padding=1),
+                            th.nn.ReLU(inplace=True),
+                            th.nn.Conv2d(128, 128, kernel_size=3, padding=1),
+                            th.nn.ReLU(inplace=True),
+                            th.nn.MaxPool2d(kernel_size=2, stride=2),
 
-                            th.nn.Conv2d(128, 256, 3, padding = 1),
-                            th.nn.ReLU(),
-                            th.nn.Conv2d(256, 256, 3, padding = 1),
-                            th.nn.ReLU(),
-                            th.nn.MaxPool2d(2, stride = 2)
+                            th.nn.Conv2d(128, 256, kernel_size=3, padding=1),
+                            th.nn.ReLU(inplace=True),
+                            th.nn.Conv2d(256, 256, kernel_size=3, padding=1),
+                            th.nn.ReLU(inplace=True),
+                            th.nn.MaxPool2d(kernel_size=2, stride=2)
                         )
 
         # state_dict = model_zoo.load_url('https://download.pytorch.org/models/vgg16-397923af.pth')
@@ -47,33 +36,42 @@ class TDNet_VGG11_Model(th.nn.Module):
             for param in self.vgg_encoder.parameters():
                 param.requires_grad = False
 
-        self.decoder = th.nn.Sequential(
-                        th.nn.Conv2d(256, 256, 3, padding = 1),
-                        th.nn.ReLU(),
-                        th.nn.ConvTranspose2d(256, 256, 4, stride = 2, padding = 1),
-
-                        th.nn.Conv2d(256, 128, 3, padding = 1),
-                        th.nn.ReLU(),
-                        th.nn.ConvTranspose2d(128, 128, 4, stride = 2, padding = 1),
-
-                        th.nn.Conv2d(128, 64, 3, padding = 1),
-                        th.nn.ReLU(),
-                        th.nn.ConvTranspose2d(64, 64, 4, stride = 2, padding = 1),
-
-                        th.nn.Conv2d(64, 1, 3, padding = 1),
-                        th.nn.ReLU()
-                    )
+        self.classifier = th.nn.Sequential(
+                            # th.nn.Dropout(0.5),  # classifier:add(nn.Dropout(0.5))
+                            th.nn.Linear(4096, 256),
+                            # th.nn.BatchNorm1d(512),  # classifier:add(nn.BatchNormalization(512))
+                            th.nn.ReLU(inplace=True),
+                            th.nn.Linear(256, num_classes)
+                        )
+        # self.decoder = th.nn.Sequential(
+        #                 th.nn.Conv2d(256, 256, 3, padding = 1),
+        #                 th.nn.ReLU(inplace=True),
+        #                 th.nn.ConvTranspose2d(256, 256, 4, stride = 2, padding = 1),
+        #
+        #                 th.nn.Conv2d(256, 128, 3, padding = 1),
+        #                 th.nn.ReLU(inplace=True),
+        #                 th.nn.ConvTranspose2d(128, 128, 4, stride = 2, padding = 1),
+        #
+        #                 th.nn.Conv2d(128, 64, 3, padding = 1),
+        #                 th.nn.ReLU(inplace=True),
+        #                 th.nn.ConvTranspose2d(64, 64, 4, stride = 2, padding = 1),
+        #
+        #                 th.nn.Conv2d(64, 1, 3, padding = 1),
+        #                 th.nn.ReLU(inplace=True)
+        #             )
 
     def forward(self, img):
         features = self.vgg_encoder(img)
-        y = self.decoder(features)
+        print(features.size())
+        features = features.view(features.size(0), -1)
+        y = self.classifier(features)
         return y
 
     def get_name(self):
-        return 'TDNet with VGG11 encoder'
+        return 'PCamNet: a VGG like model'
 
 
-class TDNet(object):
+class PCamNet(object):
 
     def __init__(self, input_dims, output_dims, learning_rate = 1e-4):
         self.input_dims = input_dims
@@ -81,8 +79,8 @@ class TDNet(object):
         self.learning_rate = learning_rate
 
     def build_model(self):
-        self.model = TDNet_VGG11_Model()
-        print (self.model)
+        self.model = PCamNetVGGModel()
+        print (self.model.get_name(), self.model)
 
     def loss_func(self, y, y_):
         loss = th.mean((y - y_) ** 2)
@@ -91,15 +89,11 @@ class TDNet(object):
     def add_optimizer(self):
         self.optimizer = th.optim.Adam(filter(lambda p: p.requires_grad, self.model.parameters()), lr = self.learning_rate)
 
-    def train_batch(self, data):
-        left_cam_data, right_cam_data, disp_data = data
-        left_cam_data, right_cam_data, disp_data = th.autograd.Variable(th.FloatTensor(left_cam_data)), \
-                                                    th.autograd.Variable(th.FloatTensor(right_cam_data)), \
-                                                    th.autograd.Variable(th.FloatTensor(disp_data))
+    def train_batch(self, data, labels):
+        y = self.model.forward( data )
 
-        y = self.model.forward( left_cam_data, right_cam_data )
-        print (y.shape, disp_data.shape)
-        loss = self.loss_func( y, disp_data )
+        print (y.shape, labels.shape)
+        loss = self.loss_func( y, labels )
 
         self.optimizer.zero_grad()
         loss.backward()
@@ -107,30 +101,19 @@ class TDNet(object):
 
         return loss.data
 
-    def compute_loss(self, data):
-        left_cam_data, right_cam_data, disp_data = data
-        left_cam_data, right_cam_data, disp_data = th.autograd.Variable(th.FloatTensor(left_cam_data)), \
-                                                    th.autograd.Variable(th.FloatTensor(right_cam_data)), \
-                                                    th.autograd.Variable(th.FloatTensor(disp_data))
-
-        y = self.model.forward( left_cam_data, right_cam_data )
-        loss = self.loss_func( y, disp_data )
+    def compute_loss(self, data, labels):
+        y = self.model.forward( data )
+        loss = self.loss_func( y, labels )
 
         return loss.data
 
     def forward_pass(self, data):
-        left_cam_data, right_cam_data, disp_data = data
-        left_cam_data, right_cam_data, disp_data = th.autograd.Variable(th.FloatTensor(left_cam_data)), \
-                                                    th.autograd.Variable(th.FloatTensor(right_cam_data)), \
-                                                    th.autograd.Variable(th.FloatTensor(disp_data))
+        y = self.model.forward( data )
+        labels = y.data
 
-        y = self.model.forward( left_cam_data, right_cam_data )
-        disparity_map = y.data
-
-        return disparity_map
+        return labels
 
     def save_model(self, ckpt_file):
-        # th.save( self.model, ckpt_file )
         self.model.save_state_dict(ckpt_file)
 
     def load_model(self, ckpt_file = None):
