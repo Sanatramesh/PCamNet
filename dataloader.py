a0283bbc3a00c26d2f0a91f97c804621f77a3a86
Sanatramesh <sanat.r@iiitb.org>
2019-03-18 21:50:14 +0530

    Added PCam dataloader

--- dataloader.py
+++ dataloader.py
@@ -1,21 +1,23 @@
 import os
+import h5py
 import pickle
 import numpy as np
 from termcolor import colored
 
 from torch.utils.data import Dataset, DataLoader
+from torchvision import transforms
 
 
 class CIFAR10Loader(Dataset):
     '''Data loader for cifar10 dataset'''
 
-    def __init__(self, data_path='data/cifar-10-batches-py', transform=None):
+    def __init__(self, data_path='data/cifar-10-batches-py', mode='train', transform=None):
         self.data_path = data_path
         self.transform = transform
-
+        self.mode = mode
         self.data = []
         self.labels = []
-        self.init_loader()
+        self._init_loader()
 
     def __len__(self):
         return len(self.labels)
@@ -30,8 +32,8 @@ class CIFAR10Loader(Dataset):
 
         return sample_train, label_train
 
-    def init_loader(self, type='train'):
-        if type == 'train':
+    def _init_loader(self):
+        if self.mode == 'train':
             batch_list = [
                 ['data_batch_1', 'c99cafc152244af753f735de768cd75f'],
                 ['data_batch_2', 'd4bba439e000b95fd0a9bffe97cbabec'],
@@ -39,10 +41,12 @@ class CIFAR10Loader(Dataset):
                 ['data_batch_4', '634d18415352ddfa80567beed471001a'],
                 ['data_batch_5', '482c414d41f54cd18b22e5b47cb7c3cb'],
             ]
-        elif type == 'test':
+        elif self.mode == 'test':
             batch_list = [
                 ['test_batch', '40351d587109b95175f43aff81a1287e'],
             ]
+        else:
+            raise Exception('Unknown: mode type(Options: train, test)')
 
         for batch in batch_list:
             print(colored('====> ', 'blue') + 'Processing file: ', os.path.join(self.data_path, batch[0]))
@@ -55,7 +59,7 @@ class CIFAR10Loader(Dataset):
         self.data = self.data.reshape(self.data.shape[0], 3, 32, 32) #.swapaxes(1, 3).swapaxes(1, 2)
 
         self.labels = np.concatenate(self.labels).astype(np.long)
-        
+
         print('Data dims, Label dims :', self.data.shape, self.labels.shape)
 
 
@@ -69,33 +73,60 @@ def unpickle(file):
 class PCamLoader(Dataset):
     '''Data loader for PCam dataset'''
 
-    def __init__(self, root_dir_img, root_dir_label, transform=None):
-        self.root_dir_img = root_dir_img
+    def __init__(self, data_path='data/', mode='train', transform=None):
+        self.data_path = data_path
         self.transform = transform
-        self.image_list_train = [f for f in os.listdir(self.root_dir_img) if os.path.isfile(os.path.join(self.root_dir_img, f))]
-        self.image_list_train.sort()
-        self.root_dir_label = root_dir_label
-        self.label_list_train = [f.replace('rs', 'mask') for f in self.image_list_train][1:]
-        self.image_list_train = self.image_list_train[1:]
+        self.mode = mode
+
+        self.data = []
+        self.labels = []
+        self._init_loader()
 
     def __len__(self):
-        return len(self.image_list_train)
+        return len(self.labels)
 
     def __getitem__(self, idx):
-        img_name_train = os.path.join(self.root_dir_img, self.image_list_train[idx])
-        label_name_train = os.path.join(self.root_dir_label, self.label_list_train[idx])
-
-        image = cv2.imread(img_name_train)
-        label_train = cv2.imread(label_name_train)
-
-        shape = image.shape
-        sample_train = image
-
-        label_train = cv2.cvtColor(label_train,cv2.COLOR_BGR2GRAY).reshape(label_train.shape[0],label_train.shape[1],1)
-        sample_train = cv2.cvtColor(sample_train,cv2.COLOR_BGR2RGB)
+        sample_train = self.data[idx]
+        label_train  = self.labels[idx]
 
         if self.transform:
             sample_train = self.transform(sample_train)
-            label_train = self.transform(label_train)
+            label_train  = self.transform(label_train)
 
         return sample_train, label_train
+
+    def _init_loader(self):
+        if self.mode == 'train':
+            batch_list = [
+                'camelyonpatch_level_2_split_train_x.h5',
+                'camelyonpatch_level_2_split_train_y.h5'
+            ]
+        elif self.mode == 'valid':
+            batch_list = [
+                'camelyonpatch_level_2_split_valid_x.h5',
+                'camelyonpatch_level_2_split_valid_y.h5'
+            ]
+        else:
+            batch_list = [
+                'camelyonpatch_level_2_split_test_x.h5',
+                'camelyonpatch_level_2_split_test_y.h5'
+            ]
+
+        data_file, label_file = batch_list
+
+        self.data = np.array(extract_hdf5(
+                                os.path.join(self.data_path, data_file)
+                                )['x'],
+                            dtype=np.float32).swapaxes(1, 3)[:,:,32:64,32:64]
+
+        self.labels = np.array(
+                            extract_hdf5(os.path.join(self.data_path, label_file))['y'],
+                              ).astype(np.long).reshape(-1)
+
+        print('Data dims, Label dims :', self.data.shape, self.labels.shape)
+        return 0
+
+
+def extract_hdf5(filename):
+    f = h5py.File(filename, 'r')
+    return f
